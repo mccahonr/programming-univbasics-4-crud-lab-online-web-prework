e7b3d861a0c7bd6bf5fb0bb2c8ac251e1a9362fa
Ryan McCahon <mccahonr@yahoo.com>
2019-08-02 16:22:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array_one = ["Pepperoni", "Sausage"]
-  array_one.push("Cheese")
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push("arrays")
 end
 
 def add_element_to_start_of_array(array, element)
