024a04be1af9777b373433861b9b734d2a0d1003
Ryan McCahon <mccahonr@yahoo.com>
2019-08-02 16:19:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array_one = ["Pepperoni", "Sausage"]
-  array_one << "Bacon"
+  array_one.push("Cheese")
 end
 
 def add_element_to_start_of_array(array, element)
