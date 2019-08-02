7ca711864939168f3ff390e113b70fdc9197efe6
Ryan McCahon <mccahonr@yahoo.com>
2019-08-02 16:17:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "Onion"
+  array_one = ["Pepperoni", "Sausage"]
+  array_one << "Bacon"
 end
 
 def add_element_to_start_of_array(array, element)
